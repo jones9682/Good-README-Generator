d266bdb8eb80186fc403c29137a47c5f5577b239
Bobby Jones Jr <jones9682@yahoo.com>
2020-07-21 11:03:50 -0400

    added questions

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        Message: "What is your email address?",
+    },
+    {
+        type: "input",
+        name: "project",
+        Message: "What is your project's name?",
+    },
+    {
+        type: "input",
+        name: "description",
+        Message: "Please write a short description of your project?",
+    },
+    {
+        type: "input",
+        name: "liscense",
+        Message: "What kind of license should your project have",
+    },
+    {
+        type: "input",
+        name: "install",
+        Message: "What command should be run to install dependencies?",
+    },
+    {
+        type: "input",
+        name: "test",
+        Message: "What command should be run to run tests?",
+    },
+    {
+        type: "input",
+        name: "repo",
+        Message: "What does the user need to know about using the repo?",
+    },
+    {
+        type: "input",
+        name: "contribute",
+        Message: "What does the user need to know about contributing to the repo?",
+    },
 ];
 
 // function to write README file
