516c7e59885266fbcb453f1ccf7b54f78ae119b8
Bobby Jones Jr <jones9682@yahoo.com>
2020-07-22 12:09:18 -0400

    added layout for README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,47 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-    return `# ${data.title}
+function generateMarkdown(data, githubInfo) {
+  return `
+# **${data.title}**
+
+## Description
+
+${data.description}
+
+## Table of contents
+
+- [GitHub Info](#GitHub)
+- [Description](#Description)
+- [Licence](#Licence)
+- [Installation](#Installation)
+- [Test](#Test)
+- [Repository Link](#Repository)
+- [Contributors](#Contributors) 
+
+## Installation
+
+${data.install}
+
+## Test
+
+${data.test}
+
+## License
+
+${data.license}
+
+## Contributors
+
+${data.contribute}
+
+## Repository
+
+- [Project Repo](${data.repo})
+
+## GitHub
+
+![Image of me](${githubInfo.githubImage})
+- [GitHub Profile](${githubInfo.profile})
+- ${githubInfo.name}
   
   `;
 }
